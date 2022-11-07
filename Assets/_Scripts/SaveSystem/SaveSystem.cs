639d04f0187e204b5eeec8624dfab76807c6ca01
Christoph2209 <chrissor1soravilla@gmail.com>
2022-11-07 10:12:04 -0500

    This is for the save system

---
+++ Assets/_Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,14 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+//Coded by Christopher Soravilla
+public class SaveSystem : MonoBehaviour
+{
+    private static string SavePath => Application.persistentDataPath + "/Saves/";
+    public static void SaveData<T>(T data, string fileName)
+    {
+        Directory.CreateDirectory()
+    }
+}
+*/
