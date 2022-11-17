09eb5e28b1656b53f05d8a240712e72dfbc63251
Ryan Beckett <rabeckett0301@gmail.com>
2022-11-17 17:58:34 -0500

    Update

--- Assets/_Scripts/Tutorial/TutorialLoop.cs
+++ Assets/_Scripts/Tutorial/TutorialLoop.cs
@@ -21,12 +21,16 @@ public class TutorialLoop : MonoBehaviour
     private string Txt;
     private Vector3 Pos;
 
+    private bool UpdatingPanel;
+
     private void Start()
     {
         universeChronology = universeSimulation.universeChronology;
         universeChronology.OnSetup.AddListener(() => OnSetup());
         universeChronology.MainPhaseStart.AddListener(() => OnMainPhaseStart());
         universeChronology.CombatPhaseStart.AddListener(() => OnCombatPhaseStart());
+
+        Debug.Log(UpdatingPanel);
     }
 
 
@@ -34,13 +38,24 @@ public class TutorialLoop : MonoBehaviour
     public void OnSetup()
     {
         Debug.Log("TUTORIAL: setup phase, turn: " + universeChronology.GetTurnCount());
+        Debug.Log(UpdatingPanel);
 
         Pos = new Vector3(0, 0, 0);
         Txt = ("Welcome to Space Traders and Raiders. " +
         "It is far in the future and humanity is in a constant power struggle. " +
         "Stake your claim and conquer the universe by way of commerce…or force.");
+        UpdatePanel(Pos, Txt, 0.5f, 0.7f);
 
-        SetPanel(Pos, Txt, 0.5f, 0.7f);
+        if (UpdatingPanel)
+        {
+            Debug.Log(UpdatingPanel);
+            Pos = new Vector3(200, 285, 0);
+            Txt = ("“PlAYER 1… It’s time to claim your territory. " +
+            "PLANETS and SHIPS that you own can be modified with items called COMPONENTS. " +
+            "These COMPONENTS are created by converting RESOURCES that you must acquire across the universe. " +
+            "Move around and open the Menu on the planet you wish to be your HOME SYSTEM with this in mind.”");
+            UpdatePanel(Pos, Txt, 0.2f, 0.5f);
+        }
     }
 
 
@@ -53,14 +68,6 @@ public class TutorialLoop : MonoBehaviour
         {
             case 1:
                 Debug.Log("TUTORIAL: It's the first turn!");
-
-                Pos = new Vector3(200, 285, 0);
-                Txt = ("“PlAYER 1… It’s time to claim your territory. " +
-                "PLANETS and SHIPS that you own can be modified with items called COMPONENTS. " +
-                "These COMPONENTS are created by converting RESOURCES that you must acquire across the universe. " +
-                "Move around and open the Menu on the planet you wish to be your HOME SYSTEM with this in mind.”");
-
-                SetPanel(Pos, Txt, 0.2f, 0.5f);
                 break;
             case 2:
                 Debug.Log("TUTORIAL: It's the second turn!");
@@ -75,7 +82,7 @@ public class TutorialLoop : MonoBehaviour
         Debug.Log("TUTORIAL: combat phase, turn: " + universeChronology.GetTurnCount());
     }
 
-    private void SetPanel(Vector3 NewPos, string TextToPrint, float ScaleX, float ScaleY)
+    public void UpdatePanel(Vector3 NewPos, string TextToPrint, float ScaleX, float ScaleY)
     {
         Panel.transform.localPosition = NewPos;
         Debug.Log(Panel.transform.position);
@@ -83,6 +90,12 @@ public class TutorialLoop : MonoBehaviour
         Panel.gameObject.transform.localScale = new Vector3(ScaleX, ScaleY, 0);
 
         TxtObj.text = TextToPrint;
+
+        UpdatingPanel = false;
     }
 
+    public void StartUpdating()
+    {
+        UpdatingPanel = true;
+    }
 }
